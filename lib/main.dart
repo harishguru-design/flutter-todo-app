7ba360538526a7edb5337cc4aa49bb2856d9f7fc
Harish Guru S <harishguru613@gmail.com>
2026-05-14 12:19:32 +0530

    Refactor main app structure and add Todo functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,121 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoHome(),
+    );
+  }
+}
+
+class TodoHome extends StatefulWidget {
+  @override
+  _TodoHomeState createState() => _TodoHomeState();
+}
+
+class _TodoHomeState extends State<TodoHome> {
+
+  TextEditingController taskController = TextEditingController();
+
+  List<String> taskList = [
+    "Task 01",
+    "Task 02",
+    "Hello world",
+    "helloo",
+    "hai",
+    "task 03"
+  ];
+
+  void addTask() {
+    if (taskController.text.isNotEmpty) {
+      setState(() {
+        taskList.add(taskController.text);
+        taskController.clear();
+      });
+    }
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      taskList.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo List App"),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+
+        child: Column(
+          children: [
+
+            Row(
+              children: [
+
+                Expanded(
+                  child: TextField(
+                    controller: taskController,
+                    decoration: InputDecoration(
+                      hintText: "Enter a task",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                  onPressed: addTask,
+                  child: Text("Add"),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 15),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: taskList.length,
+
+                itemBuilder: (context, index) {
+
+                  return Row(
+                    children: [
+
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(taskList[index]),
+                        ),
+                      ),
+
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+
+                        onPressed: () {
+                          deleteTask(index);
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
